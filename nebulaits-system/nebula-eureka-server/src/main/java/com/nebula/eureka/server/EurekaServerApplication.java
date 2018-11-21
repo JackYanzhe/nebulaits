1d2b3a3d689fab872e8b67c0d22fe70c77928da5
zheyan.yan <532968876@qq.com>
2018-11-21 11:10:25 +0800

    zengjia

---
+++ nebulaits-system/nebula-eureka-server/src/main/java/com/nebula/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.nebula.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 重要的事情说三遍：
+ * 文档地址： http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/single/spring-cloud.html
+ * 服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( EurekaServerApplication.class, args );
+    }
+}
